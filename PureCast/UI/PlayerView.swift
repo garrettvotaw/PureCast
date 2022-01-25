39a07da7c15f9df79f270bd7361cb9568f228cfa
Garrett Votaw <garrettandantonetta@gmail.com>
2022-01-25 16:09:00 -0600

    Stable UI PlayerView

---
+++ PureCast/UI/PlayerView.swift
@@ -0,0 +1,130 @@
+//
+//  PlayerView.swift
+//  PureCast
+//
+//  Created by Garrett Votaw on 12/2/20.
+//  Copyright © 2020 Garrett Votaw. All rights reserved.
+//
+
+import SwiftUI
+import AVKit
+
+struct PlayerView: View {
+    
+    @ObservedObject var audioPlayer: AudioPlayer
+    @Binding var audioProgress: Double
+    @Binding var isVisible: Bool
+    
+    var songIsLoaded: Bool {
+        audioPlayer.url != nil
+    }
+    
+    var body: some View {
+        if isVisible {
+            GeometryReader { geometry in
+                    Rectangle()
+                        .foregroundColor(Color("WhiteBlack"))
+                        .frame(maxHeight: 100)
+                        .frame(maxWidth: .infinity)
+                        .overlay(
+                            VStack {
+                                ProgressionView(timeObserver: audioPlayer)
+                                    .ignoresSafeArea()
+                                PlayerControls(audioPlayer: audioPlayer)
+                                Text("\(audioPlayer.currentItem)")
+                                    .font(.callout)
+                                    .minimumScaleFactor(1.0)
+                                    .padding(.top)
+                                    .padding(.horizontal)
+                                Spacer()
+                            }
+                        )
+                        .edgesIgnoringSafeArea(.bottom)
+                
+            }
+            .frame(maxHeight: 120)
+            .edgesIgnoringSafeArea(.bottom)
+        } else {
+            EmptyView()
+        }
+    }
+  
+}
+
+struct PlayerControls: View {
+    @ObservedObject var audioPlayer: AudioPlayer
+    
+    var body: some View {
+        HStack {
+            Button {
+                audioPlayer.seekBack()
+            } label: {
+                Image(systemName: "gobackward.15")
+                    .resizable()
+                    .frame(width: 20, height: 23, alignment: .center)
+                    .foregroundColor(Color("BlackWhite"))
+            }
+            .padding(.trailing)
+
+            PlayButton(audioPlayer: audioPlayer)
+            
+            Button {
+                audioPlayer.seekForward()
+            } label: {
+                Image(systemName: "goforward.30")
+                    .resizable()
+                    .frame(width: 20, height: 23, alignment: .center)
+                    .foregroundColor(Color("BlackWhite"))
+            }
+            .padding(.leading)
+        }
+    }
+}
+
+struct ProgressionView: View {
+    let timeObserver: AudioPlayer
+    @State private var currentTime: TimeInterval = 0
+    @State private var currentProgress: TimeInterval = 0
+    let gradient = Gradient(colors: [Color("Accent"), Color("Color")])
+      var body: some View {
+        VStack {
+            ProgressView(value: currentProgress)
+                .onReceive(timeObserver.publisher) { time in
+                    self.currentProgress = time/timeObserver.player.currentItem!.duration.seconds
+                    self.currentTime = time
+                }
+                .accentColor(Color("BlackWhite"))
+                .animation(.easeInOut(duration: 1))
+            HStack {
+                Text("\(Utility.formatSecondsToHMS(currentTime))")
+                    .padding(.leading)
+                Spacer()
+                if let time = timeObserver.player.currentItem?.duration {
+                    Text("\(Utility.formatSecondsToHMS(time.seconds))")
+                        .padding(.trailing)
+                } else {
+                    EmptyView()
+                }
+            }
+        }
+      }
+
+}
+
+
+struct PlayButton: View {
+    @ObservedObject var audioPlayer: AudioPlayer
+    
+    var body: some View {
+        Button(action: {
+            audioPlayer.playPause()
+        }, label: {
+            Image(systemName: audioPlayer.isPlaying ? "pause.fill" : "play.fill")
+                .resizable()
+                .aspectRatio(1.0, contentMode: .fit)
+                .frame(width: 25, height: 27, alignment: .center)
+                .foregroundColor(Color("BlackWhite"))
+                .padding(.horizontal)
+        })
+    }
+}
---
+++ PureCast/UI/TabView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  PureCast
+//
+//  Created by Garrett Votaw on 10/28/20.
+//  Copyright © 2020 Garrett Votaw. All rights reserved.
+//
+
+import SwiftUI
+import FeedKit
+import AVKit
+
+struct ContentView: View {
+ 
+    @State private var audioPlayer: AudioPlayer = AudioPlayer()
+    
+    var body: some View {
+        VStack {
+            FeedsListView(audioPlayer: audioPlayer)
+            PlayerView(audioPlayer: audioPlayer, audioProgress: $audioPlayer.progress, isVisible: $audioPlayer.showPlayer)
+        }
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
